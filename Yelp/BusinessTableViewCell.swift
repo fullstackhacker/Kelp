00d2bf706466fc38ace53794e64c0e49e7988084
kapadiamush <kapadiamush@gmail.com>
2017-09-22 06:10:24 -0700

    uncommit pod files

---
+++ Yelp/BusinessTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessTableViewCell.swift
+//  Yelp
+//
+//  Created by Mushaheed Kapadia on 9/21/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
